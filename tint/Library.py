dbd5fdedeaa16e643c0ac7e73bff8e1b7a533afb
Christian Friedow <christian@friedow.com>
2019-12-22 14:56:30 +0100

    Add palette creation and deletion functionality

--- tint/Library.py
+++ tint/Library.py
@@ -1,4 +1,5 @@
 from Palette import Palette
+from Color import Color
 
 class Library(object):
 
@@ -14,12 +15,29 @@ class Library(object):
         with open(self.path, 'r') as libraryFile:
             libraryJson = json.load(libraryFile)
 
+        for paletteJson in libraryJson['palettes']:
+            paletteName = paletteJson['name']
+            paletteColors = [Color(colorJson) for color in paletteJson['colors']]
+            Palette(library, paletteName, paletteColors)
+
         self.palettes = [Palette(paletteJson) for paletteJson in libraryJson['palettes']]
         
     def __create(self):
         self.palettes = []
-        self.__save()
+        self.save()
 
-    def __save(self):
+    def save(self):
         with open(self.path, 'w+') as libraryFile:
             json.dump(self.__dict__, libraryFile)
+    
+    def addPalette(self, palette):
+        self.palettes.append(palette)
+        self.save()
+    
+    def findPalettes(self, searchString):
+        return [palette for palette in self.palettes if seachString in palette.name]
+    
+    def removePalette(self, palette):
+        if palette in self.palettes:
+            self.palettes.remove(palette)
+            library.save()
--- tint/Palette.py
+++ tint/Palette.py
@@ -2,12 +2,14 @@ from Color import Color
 
 class Palette(object):
 
-    def __init__(self, paletteJson):
-        self.name = paletteJson['name']
-        self.colors = [Color(colorJson) for color in paletteJson['colors']]
+    def __init__(self, library, name, colors):
+        self.name = name
+        self.colors = colors
 
     def addColor(self, color):
         self.colors.append(color)
+        if self.library:
+            self.library.save()
     
     def findColors(self, seachString):
         return [color for color in self.colors if seachString in color.hex]
@@ -15,3 +17,4 @@ class Palette(object):
     def removeColor(self, color):
         if color in self.colors:
             self.colors.remove(color)
+            library.save()
